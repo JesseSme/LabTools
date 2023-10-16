649783ea6e61fc61be23371edbad5d13cc487c02
Jesse <jesse.smedberg@outlook.com>
2023-10-17 01:51:05 +0300

    Init commit

---
+++ Packages/LabDevice/LabDeviceClass.py
@@ -0,0 +1,26 @@
+
+class RNDLabDevice:
+
+    def __init__(self, serial):
+        self.serial = serial
+
+        # Read device id
+        # Set device id as name
+        # Fetch instructions available for device
+        # Add those instructions as functions in this class
+        return
+
+    def read(self):
+        return
+
+    def write(self):
+        return
+
+    def info(self):
+        return
+
+    def set_serial(self):
+        return
+
+    def set_device(self):
+        return
